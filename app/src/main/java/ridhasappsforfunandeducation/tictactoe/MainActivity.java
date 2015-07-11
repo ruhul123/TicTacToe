42c5a724543d040b467afc92ee2e5555a3443db0
ruhul123 <ruhul.chowdhury@live.com>
2015-07-11 15:14:26 -0400

    Methods added.

--- app/src/main/java/ridhasappsforfunandeducation/tictactoe/MainActivity.java
+++ app/src/main/java/ridhasappsforfunandeducation/tictactoe/MainActivity.java
@@ -14,10 +14,12 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
 
 
-    public Button a1, a2,a3,b1,b2,b3,c1,c2,c3;
-    public EditText enternameO,enternameX;
-    public TextView Player_O,Player_X,Player_indicator;
-    public Button A;
+    public Button a1, a2, a3, b1, b2, b3, c1, c2, c3;
+    public EditText enternameO, enternameX;
+    public TextView Player_O, Player_X, Player_indicator;
+    public Button reset;
+    public String playerSymbol;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,9 @@ public class MainActivity extends ActionBarActivity {
         Player_O = (TextView) findViewById(R.id.Player_O);
         Player_X = (TextView) findViewById(R.id.Player_X);
         Player_indicator = (TextView) findViewById(R.id.Player_indicator);
+        reset = (Button) findViewById(R.id.reset);
+        Player_indicator.setText("Player O");
+
     }
 
     @Override
@@ -61,173 +66,319 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public boolean col1(String plI) {
 
-    public void a1(View view) {
-        if (a1.getText() == "") {
-            if (Player_indicator.getText() == "Player O") {
-                a1.setText("O");
-                if ((a2.getText() == "O") && (a3.getText() == "O")) {
-
-                    String playerOname = enternameO.getText().toString();
-                    Player_indicator.setText(playerOname + "WINS!!!");
-                } else if ((b1.getText() == "O") && (c1.getText() == "O")) {
-                    String playerOname = enternameO.getText().toString();
-                    Player_indicator.setText(playerOname + "WINS!!!");
-                } else if ((b2.getText() == "O") && (c3.getText() == "O")) {
-                    String playerOname = enternameO.getText().toString();
-                    Player_indicator.setText(playerOname + "WINS!!!");
-                }
-
-
-                Player_indicator.setText("Player X");
-
-            }else if (Player_indicator.getText() == "Player X") {
-                a1.setText("x");
-                if ((a2.getText() == "x") && (a3.getText() == "x")) {
-
-                    String playerXname = enternameX.getText().toString();
-                    Player_indicator.setText(playerXname + "WINS!!!");
-                }
-            }
-
-            Player_indicator.setText("Player O");
-
-        }else{
-            String illegal_move = ("that is an illegal move!");
-            Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
-
+        if ((a1.getText() == plI) && (b1.getText() == plI) && (c1.getText() == plI)) {
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    public boolean col2(String plI) {
+
+        if ((a2.getText() == plI) && (b2.getText() == plI) && (c2.getText() == plI)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean col3(String plI) {
+
+        if ((a3.getText() == plI) && (b3.getText() == plI) && (c3.getText() == plI)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean row1(String plI) {
+
+        if ((a1.getText() == plI) && (a2.getText() == plI) && (a3.getText() == plI)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean row2(String plI) {
+
+        if ((b1.getText() == plI) && (b2.getText() == plI) && (b3.getText() == plI)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean row3(String plI) {
+
+        if ((c1.getText() == plI) && (c2.getText() == plI) && (c3.getText() == plI)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean dialeftright(String plI) {
+
+        if ((a1.getText() == plI) && (b2.getText() == plI) && (c3.getText() == plI)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean diarightleft(String plI) {
+
+        if ((a3.getText() == plI) && (b2.getText() == plI) && (c1.getText() == plI)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void getWinString(String plI) {
+        String playername;
+
+  if (plI=="O"){
+            playername = enternameO.getText().toString();
+        }else {
+            playername = enternameX.getText().toString();
+        }
+        Player_indicator.setText(playername + " WINS!!!");
     }
 
 
 
+    public void a1(View view) {
+        String aa1 = a1.getText().toString();
+
+
+        if (aa1 == "") {
+            if (Player_indicator.getText() == "Player O") {
+                a1.setText("O");
+                Player_indicator.setText("Player X");
+
+            } else if (Player_indicator.getText() == "Player X") {
+                a1.setText("x");
+                Player_indicator.setText("Player O");
+
+            }
+            aa1 = a1.getText().toString();
+            if ((row1(aa1) == true) || (col1(aa1) == true) || (dialeftright(aa1) == true)) {
+                getWinString(aa1);
+
+            }
+
+        } else {
+            String illegal_move = ("that is an illegal move!");
+            Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
+
+        }
+
+
+    }
+
 
     public void a2(View view) {
-        if (a2.getText() == "") {
+        String aa2 = a2.getText().toString();
+        if (aa2 == "") {
             if (Player_indicator.getText() == "Player O") {
                 a2.setText("O");
                 Player_indicator.setText("Player X");
-            }
-            else if(Player_indicator.getText() == "Player X"){
+            } else if (Player_indicator.getText() == "Player X") {
                 a2.setText("x");
                 Player_indicator.setText("Player O");
+
             }
-        }
-        else{
-            String illegal_move=("that is an illegal move!");
+            aa2 = a2.getText().toString();
+            if ((row1(aa2) == true) || (col2(aa2) == true)) {
+                getWinString(aa2);
+            }
+        } else {
+            String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
         }
     }
 
     public void a3(View view) {
-        if (a3.getText() == "") {
+        String aa3 = a3.getText().toString();
+        if (aa3 == "") {
             if (Player_indicator.getText() == "Player O") {
                 a3.setText("O");
                 Player_indicator.setText("Player X");
-            }
-            else if(Player_indicator.getText() == "Player X"){
+            } else if (Player_indicator.getText() == "Player X") {
                 a3.setText("x");
                 Player_indicator.setText("Player O");
+
             }
-        }
-        else{
-            String illegal_move=("that is an illegal move!");
+            aa3 = a3.getText().toString();
+            if ((row1(aa3) == true) || (col3(aa3) == true) && (diarightleft(aa3) == true)) {
+                getWinString(aa3);
+            }
+        } else {
+            String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
-    }
+        }
+
     }
 
     public void b1(View view) {
-        if (b1.getText() == "") {
+        String bb1 = b1.getText().toString();
+        if (bb1 == "") {
             if (Player_indicator.getText() == "Player O") {
                 b1.setText("O");
                 Player_indicator.setText("Player X");
             } else if (Player_indicator.getText() == "Player X") {
                 b1.setText("x");
                 Player_indicator.setText("Player O");
+
+            }
+            bb1 = b1.getText().toString();
+            if ((row2(bb1) == true) || (col1(bb1) == true)) {
+                getWinString(bb1);
             }
         } else {
             String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
         }
+
     }
 
     public void b2(View view) {
-        if (b2.getText() == "") {
+        String bb2 = b2.getText().toString();
+        if (bb2 == "") {
             if (Player_indicator.getText() == "Player O") {
                 b2.setText("O");
                 Player_indicator.setText("Player X");
             } else if (Player_indicator.getText() == "Player X") {
                 b2.setText("x");
                 Player_indicator.setText("Player O");
+
+            }
+            bb2 = b2.getText().toString();
+            if ((row2(bb2) == true) || (col2(bb2) == true)) {
+                getWinString(bb2);
             }
         } else {
             String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
         }
+
     }
 
 
     public void b3(View view) {
-        if (b3.getText() == "") {
+        String bb3 = b3.getText().toString();
+        if (bb3 == "") {
             if (Player_indicator.getText() == "Player O") {
                 b3.setText("O");
                 Player_indicator.setText("Player X");
             } else if (Player_indicator.getText() == "Player X") {
                 b3.setText("x");
                 Player_indicator.setText("Player O");
+
+            }
+            bb3 = b3.getText().toString();
+            if ((row2(bb3) == true) || (col3(bb3) == true)) {
+                getWinString(bb3);
             }
         } else {
             String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
         }
+
     }
 
 
     public void c1(View view) {
-        if (c1.getText() == "") {
+        String cc1 = c1.getText().toString();
+        if (cc1 == "") {
             if (Player_indicator.getText() == "Player O") {
                 c1.setText("O");
                 Player_indicator.setText("Player X");
             } else if (Player_indicator.getText() == "Player X") {
                 c1.setText("x");
                 Player_indicator.setText("Player O");
+
+            }
+            cc1 = c1.getText().toString();
+            if ((row3(cc1) == true) || (col1(cc1) == true) && (dialeftright(cc1) == true)) {
+                getWinString(cc1);
             }
         } else {
             String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
         }
+
     }
 
 
     public void c2(View view) {
-        if (c2.getText() == "") {
+        String cc2 = c2.getText().toString();
+        if (cc2 == "") {
             if (Player_indicator.getText() == "Player O") {
                 c2.setText("O");
                 Player_indicator.setText("Player X");
             } else if (Player_indicator.getText() == "Player X") {
                 c2.setText("x");
                 Player_indicator.setText("Player O");
+
+            }
+            cc2 = c2.getText().toString();
+            if ((row3(cc2) == true) || (col2(cc2) == true)) {
+                getWinString(cc2);
             }
         } else {
             String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
         }
+
     }
 
 
     public void c3(View view) {
-        if (c3.getText() == "") {
+        String cc3 = c3.getText().toString();
+        if (cc3 == "") {
             if (Player_indicator.getText() == "Player O") {
                 c3.setText("O");
                 Player_indicator.setText("Player X");
             } else if (Player_indicator.getText() == "Player X") {
                 c3.setText("x");
                 Player_indicator.setText("Player O");
+
+            }
+            cc3 = c3.getText().toString();
+            if ((row3(cc3) == true) || (col3(cc3) == true) && (dialeftright(cc3) == true)) {
+                getWinString(cc3);
             }
         } else {
             String illegal_move = ("that is an illegal move!");
             Toast.makeText(this, illegal_move, Toast.LENGTH_SHORT).show();
         }
+
     }
+
+    public void reset(View view) {
+        a1.setText("");
+        a2.setText("");
+        a3.setText("");
+        b1.setText("");
+        b2.setText("");
+        b3.setText("");
+        c1.setText("");
+        c2.setText("");
+        c3.setText("");
+        Player_indicator.setText("Player O");
+
     }
+}
+
+
+
+
+
+
+
 
